18e4b9c98f1e972741ae4b44be124bda3745523c
Miguel Piña <miguelepq@gmail.com>
2020-11-20 01:13:48 -0300

    create view products component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewProductsComponent
   ],
   imports: [
     BrowserModule,
